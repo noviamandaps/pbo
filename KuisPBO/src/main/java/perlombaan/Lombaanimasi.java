1a41d93d6a7ea9754023ec8881809f27e6eb8edc
noviamandaps <100067693+noviamandaps@users.noreply.github.com>
2022-04-05 12:31:15 +0700

    Update Lombaanimasi.java

--- KuisPBO/src/main/java/perlombaan/Lombaanimasi.java
+++ KuisPBO/src/main/java/perlombaan/Lombaanimasi.java
@@ -14,29 +14,43 @@ public class Lombaanimasi implements hitungnilai{
     public Lombaanimasi(double alur, double kreatif, double konten, double sinema) {
         this.alur = alur;
         this.kreatif = kreatif;
-        this.konten = kreatif;
+        this.konten = konten;
         this.sinema = sinema;
     }
 
     public double getAlur() {
         return alur;
     }
-    
+    public void setAlur(double alur){
+        this.alur=alur;
+    }
     
     public double getKonten() {
         return konten;
+    }
+    public void setKonten(double konten) {
+        this.konten = konten;
     }
     
     public double getKreatif(){
         return kreatif;
     }
+    public void setKreatif(double kreatif){
+        this.kreatif=kreatif;
+    }
     
     public double getSinema(){
         return sinema;
     }
-    @Override
-    public void nilaiakhir() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setSinema(double sinema){
+        this.sinema=sinema;
     }
-    
+
+    @Override
+    public double nilaiakhir() {
+        return (this.getAlur()*15/100)+(this.getKonten()*35/100)+(this.getKreatif()*35/100)+(this.getSinema()*15/100);
+        
+    }
+
 }
+
